0f65cb831e6227cb29cf99af772550aee9010f11
Rocco <roccobosio@gmail.com>
2022-10-13 04:05:40 -0300

    initial commit

---
+++ api-tp-ad/src/app.module.ts
@@ -0,0 +1,29 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+        synchronize: Boolean(configService.get('DB_SYNC')),
+      }),
+      inject: [ConfigService],
+    })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
